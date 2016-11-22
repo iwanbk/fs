d7e686adefb4824b2d52b7406fd57c9e8c3cd651
Muhamad Azmy <muhamad.azmy@codescalers.com>
2016-11-22 10:23:53 +0200

    Reimplement the ipfs stor to avoid hanging filesystem

--- storage/ipfsstor.go
+++ storage/ipfsstor.go
@@ -1,27 +1,80 @@
 package storage
 
 import (
-	"github.com/ipfs/go-ipfs-api"
+	"context"
+	"fmt"
 	"io"
+	"net/http"
 	"net/url"
+	"path"
+	"time"
 )
 
 type ipfsStor struct {
-	shell *shell.Shell
+	url    string
+	client *http.Client
 }
 
 func NewIPFSStorage(u *url.URL) (Storage, error) {
 	us := url.URL{
 		Scheme: "http",
 		Host:   u.Host,
-		Path:   u.Path,
+		Path:   path.Join(u.Path, "api", "v0"),
 	}
 
 	return &ipfsStor{
-		shell: shell.NewShell(us.String()),
+		url:    us.String(),
+		client: &http.Client{},
 	}, nil
 }
 
 func (s *ipfsStor) Get(hash string) (io.ReadCloser, error) {
-	return s.shell.Cat(hash)
+	request, err := http.NewRequest("POST",
+		fmt.Sprintf("%s/cat/%s", s.url, hash), nil,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	/* NOTE:
+	   We do the following trick to be able to timeout the request ONLY if
+	   no response is coming back from ipfs server. But when the data is
+	   starting to flow back, we should not timeout even if downloading the
+	   file is taking long time (if the file is really big)
+
+	   Problem with setting client.Timeout will cancel the request even if
+	   the file is being downloaded but taking longer time that the timeout
+	*/
+
+	ctx, cancel := context.WithCancel(context.Background())
+	request = request.WithContext(ctx)
+	var response *http.Response
+	wait := make(chan int)
+	defer close(wait)
+
+	go func() {
+		response, err = s.client.Do(request)
+		wait <- 1
+	}()
+
+loop:
+	for {
+		select {
+		case <-wait:
+			break loop
+		case <-time.After(15 * time.Second):
+			cancel()
+		}
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("response error: %s", response.Status)
+	}
+
+	return response.Body, nil
 }
